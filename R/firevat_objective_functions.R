fc28a51fbbbc25c5298514737c9d27f53881cfe4
Andy Jinseok Lee <jinseok@umich.edu>
2019-03-10 17:24:12 +0900

    Mar 10 2019 - more objective functions added and bugs fixed

--- R/firevat_objective_functions.R
+++ R/firevat_objective_functions.R
@@ -46,7 +46,7 @@ Euc.Obj.Fn <- function(C.refined, A.refined, C.artifactual, A.artifactual) {
 }
 
 
-#' @title Exp.Weighted.Obj.Fn
+#' @title Exp.Weighted.Obj.Fn.1
 #' @description Calculates the exponentially weighted objective value
 #' for FIREVAT GA optimization.
 #'
@@ -58,7 +58,7 @@ Euc.Obj.Fn <- function(C.refined, A.refined, C.artifactual, A.artifactual) {
 #' @return A numeric value between 0 and 1.
 #'
 #' @export
-Exp.Weighted.Obj.Fn <- function(C.refined, A.refined, C.artifactual, A.artifactual) {
+Exp.Weighted.Obj.Fn.1 <- function(C.refined, A.refined, C.artifactual, A.artifactual) {
     # Logarithmic weight
     # y = log10(0.9x + 0.1) + 1
     #
@@ -71,7 +71,37 @@ Exp.Weighted.Obj.Fn <- function(C.refined, A.refined, C.artifactual, A.artifactu
     C.artifactual <- log(((0.9* C.artifactual) + 0.1), base = 10) + 1
     A.artifactual <- (10**(A.artifactual - 1) - 0.1) / 0.9
 
-    obj.val <- C.refined * (1 - A.refined) * C.artifactual * A.artifactual
+    obj.val <- C.refined * A.refined * C.artifactual * A.artifactual
+    return(obj.val)
+}
+
+
+#' @title Exp.Weighted.Obj.Fn.2
+#' @description Calculates the exponentially weighted objective value
+#' for FIREVAT GA optimization.
+#'
+#' @param C.refined A numeric value between 0 and 1.
+#' @param A.refined A numeric value between 0 and 1.
+#' @param C.artifactual A numeric value between 0 and 1.
+#' @param A.artifactual A numeric value between 0 and 1.
+#'
+#' @return A numeric value between 0 and 1.
+#'
+#' @export
+Exp.Weighted.Obj.Fn.2 <- function(C.refined, A.refined, C.artifactual, A.artifactual) {
+    # Logarithmic weight
+    # y = 0.5 * log10(0.99x + 0.01) + 1
+    #
+    # Exponential weight
+    # y = (10^(2*(x-1)) - 0.01) / 0.99
+
+    C.refined <- (0.5 * log(((0.99 * C.refined) + 0.01), base = 10)) + 1
+    A.refined <- (10**(2*((1 - A.refined) - 1)) - 0.01) / 0.99
+
+    C.artifactual <- (0.5 * log(((0.99 * C.artifactual) + 0.01), base = 10)) + 1
+    A.artifactual <- (10**(2*(A.artifactual - 1)) - 0.01) / 0.99
+
+    obj.val <- C.refined * A.refined * C.artifactual * A.artifactual
     return(obj.val)
 }
 
@@ -110,5 +140,96 @@ Euc.Exp.Weighted.Obj.Fn <- function(C.refined, A.refined, C.artifactual, A.artif
     return(obj.val)
 }
 
+
+#' @title Euc.Exp.Weighted.Seq.Art.Only.Obj.Fn.1
+#' @description
+#' Calculates the Euclidean-distance of logarithmically weighted
+#' objective value for FIREVAT GA optimization.
+#'
+#' @param C.refined A numeric value between 0 and 1.
+#' @param A.refined A numeric value between 0 and 1.
+#' @param C.artifactual A numeric value between 0 and 1.
+#' @param A.artifactual A numeric value between 0 and 1.
+#'
+#' @return A numeric value between 0 and 1.
+#'
+#' @export
+Euc.Exp.Weighted.Seq.Art.Only.Obj.Fn.1 <- function(C.refined, A.refined, C.artifactual, A.artifactual) {
+    # Logarithmic weight
+    # y = log10(0.9x + 0.1) + 1
+    #
+    # Exponential weight
+    # y = (10^(x-1) - 0.1) / 0.9
+    #
+    # Calculates the Euclidean distance from the reference point
+
+    A.refined <- (10**((1 - A.refined) - 1) - 0.1) / 0.9
+    A.artifactual <- (10**(A.artifactual - 1) - 0.1) / 0.9
+
+    reference.point <- c(0, 0)
+    curr.point <- c(A.refined, A.artifactual)
+    obj.val <- as.numeric(dist(rbind(reference.point, curr.point)))
+    return(obj.val)
+}
+
+
+#' @title Euc.Exp.Weighted.Seq.Art.Only.Obj.Fn.2
+#' @description
+#' Calculates the Euclidean-distance of logarithmically weighted
+#' objective value for FIREVAT GA optimization.
+#'
+#' @param C.refined A numeric value between 0 and 1.
+#' @param A.refined A numeric value between 0 and 1.
+#' @param C.artifactual A numeric value between 0 and 1.
+#' @param A.artifactual A numeric value between 0 and 1.
+#'
+#' @return A numeric value between 0 and 1.
+#'
+#' @export
+Euc.Exp.Weighted.Seq.Art.Only.Obj.Fn.2 <- function(C.refined, A.refined, C.artifactual, A.artifactual) {
+    # Logarithmic weight
+    # y = log10(0.9x + 0.1) + 1
+    #
+    # Exponential weight
+    # y = (10^(x-1) - 0.1) / 0.9
+    #
+    # Calculates the Euclidean distance from the reference point
+
+    C.artifactual <- log(((0.9 * C.artifactual) + 0.1), base = 10) + 1
+    A.artifactual <- (10**(A.artifactual - 1) - 0.1) / 0.9
+
+    reference.point <- c(0,0)
+    curr.point <- c(C.artifactual, A.artifactual)
+    obj.val <- as.numeric(dist(rbind(reference.point, curr.point)))
+    return(obj.val)
+}
+
+
+#' @title Exp.Weighted.Refined.Seq.Art.Only.Obj.Fn
+#' @description
+#' Calculates the Euclidean-distance of logarithmically weighted
+#' objective value for FIREVAT GA optimization.
+#'
+#' @param C.refined A numeric value between 0 and 1.
+#' @param A.refined A numeric value between 0 and 1.
+#' @param C.artifactual A numeric value between 0 and 1.
+#' @param A.artifactual A numeric value between 0 and 1.
+#'
+#' @return A numeric value between 0 and 1.
+#'
+#' @export
+Exp.Weighted.Refined.Seq.Art.Only.Obj.Fn <- function(C.refined, A.refined, C.artifactual, A.artifactual) {
+    # Logarithmic weight
+    # y = log10(0.9x + 0.1) + 1
+    #
+    # Exponential weight
+    # y = (10^(x-1) - 0.1) / 0.9
+    #
+    # Calculates the Euclidean distance from the reference point
+
+    A.refined <- (10**((1 - A.refined) - 1) - 0.1) / 0.9
+    obj.val <- A.refined
+    return(obj.val)
+}
 
 
